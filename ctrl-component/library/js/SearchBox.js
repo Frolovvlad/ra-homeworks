38fb30455a4a2dd7f0464f6383f7c78247ffb369
Vladislav Frolov <frolovvlad1995@gmail.com>
2018-07-30 12:28:57 +0300

    version_1.0

--- ctrl-component/library/js/SearchBox.js
+++ ctrl-component/library/js/SearchBox.js
@@ -1,5 +1,32 @@
-const SearchBox = () => {
+/* 
+class SearchBox extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            query: this.props.value,
+        }
+    }
+
+    searchText = event => {
+        this.props.filterBooks(event.currentTarget.value)
+    }
+
+    render() {
+        return (
+            <input type="text" placeholder="Поиск по названию или автору" value={this.props.value} onChange={this.searchText}/>
+        );
+    }
+}
+ */
+
+
+
+const SearchBox = (props) => {
+    const searchText = event => {
+        props.filterBooks(event.currentTarget.value)
+    }
+    
     return (
-        <input type="text" placeholder="Поиск по названию или автору"/>
+        <input type="text" placeholder="Поиск по названию или автору" value={props.value} onChange={searchText}/>
     );
-};
+}
